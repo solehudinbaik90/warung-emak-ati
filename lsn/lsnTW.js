25d1b48d093315552f0f80c75fc2b2cda354a90c
Muhamad Soleh <solehudinz3692@gmail.com>
2026-09-06 17:09:57 +0700

    Update twLSN array and cekLSN function
    Refactor cekLSN function and update twLSN array with a specific blog URL.

--- lsn/lsnTW.js
+++ lsn/lsnTW.js
@@ -1,7 +1,2 @@
-var twLSN = [];
-
-function cekLSN(value, arr){ 
-    return true; 
-} 
-
-var hst = window.location.hostname;
+var twLSN = ['warung-emak-ati-rt12.blogspot.com'];
+function cekLSN(value,arr){ var status = false; for(var i=0; i<arr.length; i++){ var name = arr[i]; if(name == value){ status = true; break; } } return status; } var hst = window.location.hostname; if(cekLSN(hst, twLSN) == false) { window.location.href = 'https://warung-emak-ati-rt12.blogspot.com'; }
